1a55d47d8d1aa996069b045f7ef288ab1de50f14
Muhammet Vural <muhammetvural94@gmail.com>
2024-12-22 16:31:54 +0300

    todo app riverpod ile desteklenerek tamamlanmıstır

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,19 @@ import 'package:first_project/pages/homepage/home_page.dart';
 import 'package:first_project/providers/agirlik_state.dart';
 import 'package:first_project/router/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:provider/provider.dart';
 
 Future<void> main() async {
-  runApp( EasyLocalization(
-      supportedLocales: [Locale('en'), Locale('tr')],
-      path: 'assets/language', // <-- change the path of the translation files 
-      fallbackLocale: Locale('tr'),
-      saveLocale: true,
-      startLocale: Locale('tr'),
-    child: MyApp()));
+  runApp( ProviderScope(
+    child: EasyLocalization(
+        supportedLocales: [Locale('en'), Locale('tr')],
+        path: 'assets/language', // <-- change the path of the translation files 
+        fallbackLocale: Locale('tr'),
+        saveLocale: true,
+        startLocale: Locale('tr'),
+      child: MyApp()),
+  ));
     WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
 }
@@ -24,18 +27,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [ChangeNotifierProvider(create: (context) => AgirlikState())],
-      child: MaterialApp.router(
-        localizationsDelegates: context.localizationDelegates,
-        supportedLocales: context.supportedLocales,
-        locale: context.locale,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        routerConfig: _appRoute.config(),
+    return MaterialApp.router(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      routerConfig: _appRoute.config(),
     );
   }
 }
--- lib/providers/todo_provider.dart
+++ lib/providers/todo_provider.dart
@@ -3,11 +3,28 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../models/todo_model.dart';
 
-class TodoProvider extends StateNotifier<List<TodoModel>> {
-   TodoProvider() : super([]);
+class TodoNotifier extends StateNotifier<List<TodoModel>> {
+   TodoNotifier() : super([]);
 
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+   void addTodo (String title){
+    state = [
+      ...state,
+      TodoModel(id: DateTime.now().toString(), title: title),
+    ];
+   }
+
+   void toggleTodo(String id){
+    state = state.map((todo){
+      if (todo.id == id) {
+        return todo.copyWith(isComplated: !todo.isComplated);
+      }
+      else { return todo;}
+    }).toList();
+   }
+
+   void deleteTodo (String id){
+    state = state.where((todo) => todo.id != id).toList();
+   }
+
   }
-}
+  final todoProvider = StateNotifierProvider<TodoNotifier, List<TodoModel>>((ref) => TodoNotifier());
